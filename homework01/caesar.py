1c93296debbbceaf7ec8574c882ad7c37f5ba712
XDM1444 <kirka2282007@gmail.com>
2025-10-27 23:05:13 +0300

    Реализована функция encrypt_caesar и decrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -12,6 +12,13 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     ciphertext = ""
     # PUT YOUR CODE HERE
+    for ch in plaintext:
+        if "A" <= ch <= "Z":
+            ciphertext += chr((ord(ch) - ord("A") + shift) % 26 + ord("A"))
+        elif "a" <= ch <= "z":
+            ciphertext += chr((ord(ch) - ord("a") + shift) % 26 + ord("a"))
+        else:
+            ciphertext += ch
     return ciphertext
 
 
@@ -29,4 +36,11 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     plaintext = ""
     # PUT YOUR CODE HERE
+    for ch in ciphertext:
+        if "A" <= ch <= "Z":
+            plaintext += chr((ord(ch) - ord("A") - shift) % 26 + ord("A"))
+        elif "a" <= ch <= "z":
+            plaintext += chr((ord(ch) - ord("a") - shift) % 26 + ord("a"))
+        else:
+            plaintext += ch
     return plaintext
