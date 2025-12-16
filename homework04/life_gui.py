62a63b9f993b027f02bcadbc88847d3edeab6479
XDM1444 <kirka2282007@gmail.com>
2025-12-16 16:55:36 +0300

    8 plz

--- homework04/life_gui.py
+++ homework04/life_gui.py
@@ -17,10 +17,11 @@ class GUI(UI):
         self.paused = False
 
     def draw_lines(self) -> None:
+        color = pygame.Color("black")
         for x in range(0, self.width, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
+            pygame.draw.line(self.screen, color, (x, 0), (x, self.height))
         for y in range(0, self.height, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))
+            pygame.draw.line(self.screen, color, (0, y), (self.width, y))
 
     def draw_grid(self) -> None:
         for r in range(self.life.rows):
@@ -67,3 +68,6 @@ class GUI(UI):
             clock.tick(self.speed)
 
         pygame.quit()
+
+    def step(self) -> None:
+        self.life.step()
